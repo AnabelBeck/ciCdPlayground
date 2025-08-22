2f9e8990e8ac18982c1f5883cbeac9f8acd43100
AnabelBeck <anabel.beck@tngtech.com>
2025-08-22 15:07:45 +0200

    TRy fix again app.cy.js

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,9 +1,9 @@
 // app.spec.js created with Cypress
 //
 describe('The app', () => {
-    it('tells that you are a butler', () => {
-        cy.visit('')
-        cy.get('h1')
+it('tells that you are a butler', () => {
+cy.visit('')
+cy.get('h1')
         .should('have.text', 'Hello, my name is Anabel Beck!')
-    })
-  })
+})
+})
