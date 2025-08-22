e034ebaf1168b164421e1d4ead9b5a62c189f036
AnabelBeck <anabel.beck@tngtech.com>
2025-08-22 14:06:09 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Anabel Beck",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["test, deploy, lint checks"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Tekton",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["get a better understanding of pipelines, learn the basics"],
   },
 });
